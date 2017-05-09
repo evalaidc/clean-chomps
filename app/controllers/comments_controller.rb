012c58c97036f0dea5801f14942f5393521ad503
Eva Lai <evalai.dc@gmail.com>
2017-05-08 22:36:44 -0400

    added create comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
-  def index
+  def new
+    @trend = Trend.find(params[:trend_id])
+    @comment = @trend.comments.new
+  end
 
+  def create
+    @trend = Trend.find(params[:trend_id])
+    @comment = @trend.comments.create(comment_params)
+    redirect_to trend_path(@trend)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author, :body, :additional_link)
   end
 end
