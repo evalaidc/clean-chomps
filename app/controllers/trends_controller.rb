4e0e84a0e09da9b81ae9bca979131489b99ddbe1
Eva Lai <evalai.dc@gmail.com>
2017-05-08 21:59:20 -0400

    created comment show functionality

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -3,8 +3,31 @@ class TrendsController < ApplicationController
     @trends = Trend.all
   end
 
+  def new
+    @trend = Trend.new
+  end
+
+  def create
+    @trend = Trend.create(trend_params)
+    redirect_to trend_path(@trend)
+  end
 
   def show
     @trend = Trend.find(params[:id])
+    @comments = @trend.comments.all
+  end
+
+  def edit
+
+  end
+
+  def updated
+
+  end
+
+
+    private
+  def trend_params
+    params.require(:trend).permit(:title, :description, :photo_url, :ref_link)
   end
 end
